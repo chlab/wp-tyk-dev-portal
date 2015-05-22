2d6ea18a86e866bceb4e3c16ba4617b949e48407
Bartosz Podlewski <b.podlewski@gmail.com>
2015-05-22 11:43:35 +0200

    added loader for MU Plugins, Added Rewrite Plugin

---
+++ ogdch.dev/content/mu-plugins/load.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    Plugin Name: MU-Plugins
+    Plugin URI:
+    Description: Contains: HM-Rewrites
+    Author: Bartosz Podlewski
+    Author URI: http://liip.ch/
+*/
+
+require WPMU_PLUGIN_DIR.'/hm-rewrite/hm-rewrites.php';
