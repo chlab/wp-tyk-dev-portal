ee1680236478a4f4bfab20ecadb7c1acfd89c817
Bartosz Podlewski <b.podlewski@gmail.com>
2015-05-28 15:02:24 +0200

    SCSS Setup, Rabbit MQ Consumer

--- ogdch.dev/rabbitmq/read.php
+++ ogdch.dev/rabbitmq/read.php
@@ -1,64 +1,99 @@
 <?php
 
-/*
-$msg = array(
-    'ref' => 123,
-    'title' => array(
-        'de' => 'titel DE',
-        'fr' => 'titel FR',
-        'en' => 'titel EN',
-        'it' => 'titel IT',
-    ),
-    'action' => 'insert',
-);*/
+define( 'WP_USE_THEMES', false );
+require_once( __DIR__ . '/../cms/wp-load.php' );
+require_once( __DIR__ . '/../vendor/autoload.php' );
 
-define('WP_USE_THEMES', false);
-require_once(__DIR__ . '/../cms/wp-load.php');
-require_once(__DIR__ . '/../vendor/autoload.php');
-
-if( ! function_exists('pll_save_post_translations')) {
-    die('pll_save_post_translations does not exist');
+if ( ! function_exists( 'pll_save_post_translations' ) ) {
+	die( 'pll_save_post_translations does not exist' );
 }
 
 use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPConnection('localhost', 5672, 'admin', '123');
-$channel = $connection->channel();
+$connection = new AMQPConnection( 'localhost', 5672, 'admin', '123' );
+$channel    = $connection->channel();
 
-$channel->queue_declare('ckan', false, false, false, false);
+$channel->queue_declare( 'ckan', false, false, false, false );
 
-echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+echo '[*] Waiting for messages. To exit press CTRL+C', "\n";
 
-$callback = function($msg) {
-    $msg = json_decode ($msg->body );
-    $translations = array();
-    foreach ( $msg->title as $lang => $title ) {
-        var_dump($lang .' - '.$title);
+$callback = function ( $msg ) {
+	$msg          = json_decode( $msg->body );
+	$translations = array();
+	$baseaction   = $msg->action;
+
+	foreach ( $msg->title as $lang => $title ) {
+
+		$action = $msg->action;
+
+		$post = array(
+			'post_author' => 1,
+			'post_type'   => 'ckan-dataset',
+			'post_title'  => $title,
+		);
+
+		$args = array(
+			'meta_key'       => '_ckandataset_reference',
+			'meta_value'     => $msg->ref,
+			'meta_compare'   => '=',
+			'post_type'      => 'ckan-dataset',
+			'posts_per_page' => 1,
+			'cache_results'  => false,
+			'fields'         => 'ids',
+			'post_status'    => 'any',
+			'lang'           => $lang,
+		);
+
+        $res = get_posts( $args );
+		$num_res = count( $res );
 
 
-        $post = array (
-	        'post_author' => 1,
-            'post_type' => 'ckan-dataset',
-            'post_title' => $title
-        );
+		// Error handling
+		if ( $num_res === 1 && $action == 'insert' ) { // If Ref. ID exists but action is insert -> update
+			$action = 'update';
+		} elseif ( $num_res === 0 && $action == 'update' ) { // If Ref. ID does not exists but action is update -> insert
+			$action = 'insert';
+		}
 
-        $new_id = wp_insert_post ( $post, true ); // Insert Post
-        add_post_meta( $new_id, '_ckandataset_reference', $msg->ref, TRUE );
 
-        pll_set_post_language( $new_id, $lang );  // Set Langauge
+		if ( $num_res === 1 && $action == 'update' ) {
 
-        $translations[$lang] = $new_id;
+			// Post exists so update it
+			$post['ID'] = $res[0];
+			$new_id     = $res[0];
+			wp_update_post( $post );
 
-    }
+			echo "UPDATE " . $post['ID'] . " to " . $post['post_title'] . " \n";
 
-    // Connect Posts with each other
-    pll_save_post_translations( $translations );
+		} elseif ( $num_res === 0 && $action == 'insert' ) {
+
+			// Post does not exist so create it, add reference as custom field and set language of post
+			$new_id = wp_insert_post( $post, true );
+			add_post_meta( $new_id, '_ckandataset_reference', $msg->ref, true );
+			pll_set_post_language( $new_id, $lang );  // Set Langauge
+
+			echo "Insert " . $new_id . " as " . $post['post_title'] . " \n";
+
+		} else {
+			echo "ERROR\n";
+		}
+
+
+		$translations[ $lang ] = $new_id;
+
+	}
+
+	// Connect Posts with each other
+	if ( $action === 'insert' ) {
+		pll_save_post_translations( $translations );
+	}
 
 };
 
-$channel->basic_consume('ckan', '', false, true, false, false, $callback);
 
-while(count($channel->callbacks)) {
+$channel->basic_consume( 'ckan', '', false, true, false, false, $callback );
+
+while ( count( $channel->callbacks ) ) {
 	$channel->wait();
 }
--- ogdch.dev/rabbitmq/write.php
+++ ogdch.dev/rabbitmq/write.php
@@ -1,18 +1,4 @@
 <?php
-
-/*$msg = array(
-    'ref' => 123,
-    'title' => array(
-        'de' => 'titel DE',
-        'fr' => 'titel FR',
-        'en' => 'titel EN',
-        'it' => 'titel IT',
-    ),
-    'action' => 'insert',
-);
-
-    var_dump(json_encode($msg));die;*/
-
 require_once(__DIR__ . '/../vendor/autoload.php');
 
 use PhpAmqpLib\Connection\AMQPConnection;
@@ -20,21 +6,27 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 $connection = new AMQPConnection('localhost', 5672, 'admin', '123');
 $channel = $connection->channel();
+$queue_name = 'ckan';
 
-$channel->queue_declare('ckan', false, false, false, false);
+$channel->queue_declare($queue_name, false, false, false, false);
 
 $ref = rand(100, 999);
+$ref = 6588;
 $mock_ckan_data = array(
-	'{"ref":' . $ref . ',"title":{"de":"titel DE ' . $ref . '","fr":"titel FR ' . $ref . '","en":"titel EN ' . $ref . '","it":"titel IT ' . $ref . '"},"action":"insert"}',
+	'{"ref":' . $ref . ',"title":{"de":"Hans DE ' . $ref . '","fr":"Hans FR ' . $ref . '","en":"Hans EN ' . $ref . '","it":"Hans IT ' . $ref . '"},"action":"update"}',
+
+	//'{"ref":' . $ref . ',"title":{"de":"titelyolo DE ' . $ref . '","fr":"titelyolo FR ' . $ref . '","en":"titelyolo EN ' . $ref . '","it":"titelyolo IT ' . $ref . '"},"action":"insert"}',
+	//'{"ref":' . $ref . ',"title":{"de":"titelyolo DE ' . $ref . '","fr":"titelyolo FR ' . $ref . '","en":"titelyolo EN ' . $ref . '","it":"titelyolo IT ' . $ref . '"},"action":"update"}',
+	//'{"ref":' . $ref . ',"title":{"de":"titel2 DE ' . $ref . '","fr":"titel2 FR ' . $ref . '","en":"titel2 EN ' . $ref . '","it":"titel2 IT ' . $ref . '"},"action":"update"}',
+	//'{"ref":' . $ref . ',"title":{"de":"titel3 DE ' . $ref . '","fr":"titel33 FR ' . $ref . '","en":"titel333 EN ' . $ref . '","it":"titel33333 IT ' . $ref . '"},"action":"update"}',
+	//'{"ref":653,"title":{"de":"titel--3 DE ' . $ref . '","fr":"titel--33 FR ' . $ref . '","en":"titel--333 EN ' . $ref . '","it":"titel---33333 IT ' . $ref . '"},"action":"update"}',
 );
-$queue_name = 'ckan';
 
 foreach($mock_ckan_data as $ckan_data) {
 	$msg = new AMQPMessage($ckan_data);
 	$channel->basic_publish($msg, '', $queue_name);
+	echo ' [*] Sent ' . uniqid() . ' ' . count($mock_ckan_data) . ' messages to ' . $queue_name . ' queue.', "\n<br>";
 }
 
 $channel->close();
 $connection->close();
-
-echo ' [*] Sent ' . count($mock_ckan_data) . ' messages to ' . $queue_name . ' queue.', "\n";
