f6df80ad0aa90cf11cebd2e7085a948abc7b1acc
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-06-11 15:54:44 +0200

    created custom meta box to allow choosing only one ckan_group per dataset

--- ogdch.dev/content/mu-plugins/ckan-tax-sync/ckan-tax-sync.php
+++ ogdch.dev/content/mu-plugins/ckan-tax-sync/ckan-tax-sync.php
@@ -285,3 +285,69 @@ function ckan_synchronize_taxonomy( $taxonomy, $data ) {
 	return json_encode( $return );
 }
 
+
+/**
+ * Create custom meta box to allow choosing only one ckan_group per dataset.
+ * Checkbox -> Selectbox
+ */
+function ckan_organisation_custom_meta_box() {
+	remove_meta_box( 'ckan_organisationdiv', 'ckan-local-dataset', 'side' );
+	add_meta_box( 'ckan_organisationdiv',  __( 'Organisation', 'ogdch' ), 'ckan_organisation_meta_box', 'ckan-local-dataset', 'side' );
+
+	remove_meta_box( 'ckan_organisationdiv', 'ckan-dataset', 'side' );
+	add_meta_box( 'ckan_organisationdiv',  __( 'Organisation', 'ogdch' ), 'ckan_organisation_meta_box', 'ckan-dataset', 'side' );
+}
+add_action('add_meta_boxes', 'ckan_organisation_custom_meta_box');
+
+/* Prints the taxonomy box content */
+function ckan_organisation_meta_box($post) {
+	$tax_name = 'ckan_organisation';
+	?>
+	<div class="tagsdiv" id="<?php echo $tax_name; ?>">
+		<div class="jaxtag">
+			<?php
+			// Use nonce for verification
+			wp_nonce_field( plugin_basename( __FILE__ ), 'ckan_organisation_noncename' );
+			$type_IDs = wp_get_object_terms( $post->ID, 'ckan_organisation', array('fields' => 'ids') );
+			wp_dropdown_categories('taxonomy=ckan_organisation&hide_empty=0&orderby=name&name=ckan_organisation&show_option_none=' . __( 'No Organisation', 'ogdch' ) . '&selected='.$type_IDs[0]); ?>
+		</div>
+	</div>
+	<?php
+}
+
+/* When the post is saved, saves our custom taxonomy */
+function ckan_organisation_save_postdata( $post_id ) {
+	// verify if this is an auto save routine.
+	// If it is our form has not been submitted, so we dont want to do anything
+	if ( ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) || wp_is_post_revision( $post_id ) )
+		return;
+
+	// verify this came from the our screen and with proper authorization,
+	// because save_post can be triggered at other times
+
+	if ( !wp_verify_nonce( $_POST['ckan_organisation_noncename'], plugin_basename( __FILE__ ) ) )
+		return;
+
+
+	// Check permissions
+	if ( 'ckan-local-dataset' == $_POST['post_type'] )
+	{
+		if ( !current_user_can( 'edit_page', $post_id ) )
+			return;
+	}
+	else
+	{
+		if ( !current_user_can( 'edit_post', $post_id ) )
+			return;
+	}
+
+	// OK, we're authenticated: we need to find and save the data
+
+	$type_ID = $_POST['ckan_organisation'];
+
+	$type = ( $type_ID > 0 ) ? get_term( $type_ID, 'ckan_organisation' )->slug : NULL;
+
+	wp_set_object_terms(  $post_id , $type, 'ckan_organisation' );
+}
+/* Do something with the data entered */
+add_action( 'save_post', 'ckan_organisation_save_postdata' );
