a28c43a1168a568d69c705d6ff3b94eedcebc826
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2015-06-19 10:11:26 +0200

    Fix path to wp-ckan-backend plugin

--- ogdch.dev/content/mu-plugins/load.php
+++ ogdch.dev/content/mu-plugins/load.php
@@ -10,4 +10,4 @@
 require WPMU_PLUGIN_DIR.'/hm-rewrite/hm-rewrites.php';
 require WPMU_PLUGIN_DIR.'/app-directory/app-directory.php';
 require WPMU_PLUGIN_DIR.'/ckan-dataset/ckan-dataset.php';
-require WPMU_PLUGIN_DIR.'/ckan-backend/ckan-backend.php';
+require WPMU_PLUGIN_DIR.'/wp-ckan-backend/ckan-backend.php';
