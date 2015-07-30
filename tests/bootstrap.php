956cbdc08c2aa10e7fcfa685e36c643437c8c78d
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2015-07-30 17:58:58 +0200

    Fix plugin path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,7 +16,7 @@ function _manually_switch_theme() {
 tests_add_filter( 'plugins_loaded', '_manually_switch_theme' );
 
 function _manually_load_plugins() {
-	require '/tmp/wordpress/wp-content/plugins/ckan-backend.php';
+	require dirname( __FILE__ ) . '/../web/ogdch.dev/content/plugins/wp-ckan-backend/ckan-backend.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugins' );
 
