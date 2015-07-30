c291b482151bce2a723c32a7ce60ae9e0362eada
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2015-07-30 11:24:01 +0200

    Do not exit if there are error when testing/linting

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,6 +11,8 @@ curl -s https://getcomposer.org/installer | php
 php composer.phar install
 $DIR/../bin/phpcs --config-set installed_paths $DIR/../vendor/wp-coding-standards/wpcs
 
+set +e
+
 # Check code style
 $DIR/../phpcodesniffer.sh $DIR/../web/ogdch.dev/content/plugins/wp-ckan-backend/ $DIR/../web/ogdch.dev/content/themes/wp-ogdch-theme
 
