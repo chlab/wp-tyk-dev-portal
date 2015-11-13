1f7e646916524dfbbeebb0b881af30d3867a5230
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2015-11-13 13:24:31 +0100

    Re-add resetting of DB after scenario and increase timeout

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,8 +25,7 @@ echo "Exit Code: $phpcs_exit"
 cp /vagrant/web/ogdch.dev/wp-functest-config.php.dist /vagrant/web/ogdch.dev/wp-functest-config.php
 
 # run cucumber tests
-#RESET_DB=true cucumber.js --tags @finished $DIR/../tests/features --require $DIR/../tests/features
-cucumber.js --tags @finished $DIR/../tests/features --require $DIR/../tests/features
+RESET_DB=true cucumber.js --tags @finished $DIR/../tests/features --require $DIR/../tests/features
 func_exit=$?
 echo "Exit Code: $func_exit"
 
