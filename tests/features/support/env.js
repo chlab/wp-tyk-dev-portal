e5fc592c22a96d00752f7ef0887595a184989021
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2015-11-14 21:43:27 +0100

    Increase timeout to make sure all tests pass

--- tests/features/support/env.js
+++ tests/features/support/env.js
@@ -1,6 +1,6 @@
 var configure = function () {
-    // set timeout to 3 minutes
-    this.setDefaultTimeout(5 * 60 * 1000);
+    // set timeout to 10 minutes
+    this.setDefaultTimeout(10 * 60 * 1000);
 };
 
 module.exports = configure;
