7e3d05aded5da87627c1a0762abd09ac7f4d7364
Jonas Oppenländer <jonas.oppenlaender@liip.ch>
2016-06-17 10:20:11 +0200

    move ftp harvester

---
+++ web/ckanext/ckanext-harvest/ckanext/harvest/harvesters/__init__.py
@@ -0,0 +1,4 @@
+
+from ckanext.harvest.harvesters.ckanharvester import CKANHarvester
+from ckanext.harvest.harvesters.ftpharvester import FTPHarvester
+from ckanext.harvest.harvesters.base import HarvesterBase
