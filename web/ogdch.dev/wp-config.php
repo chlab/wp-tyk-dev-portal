60d2ff338f0ee7a614184c666c3f7c52877c921a
Christof Leuenberger <christof.leuenberger@liip.ch>
2016-07-04 13:59:32 +0200

    changed db table prefix for this project

--- web/ogdch.dev/wp-config.php
+++ web/ogdch.dev/wp-config.php
@@ -25,7 +25,7 @@ if ( file_exists( dirname( __FILE__ ) . '/wp-local-config.php' ) ) {
 // Table prefix
 // ===================================================
 
-$table_prefix = 'ogdch_';
+$table_prefix = 'odpch_';
 
 
 // ===================================================
